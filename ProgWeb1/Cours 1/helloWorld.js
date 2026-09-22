40e3e06add4a225470d916631c2ce1182aafaace
Beil789 <benoit.caumeil@hes-so.ch>
2026-09-22 10:00:42 +0200

    Add files via upload

---
+++ ProgWeb1/Cours 1/helloWorld.js
@@ -0,0 +1,21 @@
+function getMax(a, b, c) {
+    if (a > b, c)
+        return a;
+    if (b > a, c)
+        return b;
+    else return c;
+}
+
+const max = getMax(3, 8, 5);
+console.log(max);
+
+
+
+
+function compareA(a, b) {
+
+
+}
+
+
+
